12c659b5ab4715464831830cfff6db75be3c5151
chefbc <chefbc@icloud.com>
2020-04-30 10:56:02 -0500

    test

--- src/components/PostListing/PostListing.jsx
+++ src/components/PostListing/PostListing.jsx
@@ -47,7 +47,7 @@ class PostListing extends React.Component {
               <section className="post-excerpt">
                 {/* TODO limit excerpt to 26 words */}
                 <p>
-                  What {excerpt}{" "}
+                  {excerpt}{" "}
                   <Link className="read-more" to={path}>
                     &raquo;
                   </Link>
